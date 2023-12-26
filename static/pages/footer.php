3318f2801f577c4167c146a3febe13f07f77b574
Andrew <frintu.andrei07@gmail.com>
2023-12-26 21:50:51 +0200

    footer & navbar components

---
+++ static/pages/footer.php
@@ -0,0 +1,43 @@
+<?php
+    /*
+    
+        Website developed by Andrei Frîntu
+        Critical maintenance @ codulluiandrei.ro
+
+        Version:    3.7@main:tb
+        Stage:      published@teaborgs.ro
+        Contact:    admin@codulluiandrei.ro
+
+    */
+?>
+<div class="text-white py-5 mt-5 w-75 w-sm-100 align-items-center m-auto border-top d-flex fl-cont gap-2" id="Footer">
+    <div style="width: 25%;" class="w-sm-100 d-flex align-items-center flex-column fil">
+        <img alt="" src="/static/logo/logo-2024.png" class="img-fluid">
+        <p class="text-white text-unbounded text-center fs-5">© 2018 - 2023</p>
+    </div>
+    <div style="width: 25%;" class="w-sm-100 d-flex align-items-end text-end flex-column fir">
+        <h5><?php echo ($l == 1) ? "Rețele de socializare" : "Social media"; ?></h5>
+        <ul class="nav flex-column">
+            <li class="footer-item mb-2"><a target="_blank" href="https://www.instagram.com/tea_borgs" class="nav-link p-0 text-body-secondary">Instagram <img alt="" src="/static/icons/icons8-instagram-96.png" style="width: 32px !important; height: 32px !important;"></a></li>
+            <li class="footer-item mb-2"><a target="_blank" href="https://www.facebook.com/TeaBorgs" class="nav-link p-0 text-body-secondary">Facebook <img alt="" src="/static/icons/icons8-facebook-96.png" style="width: 32px !important; height: 32px !important;"></a></li>
+            <li class="footer-item mb-2"><a target="_blank" href="https://www.tiktok.com/@teaborgs.19121" class="nav-link p-0 text-body-secondary">TikTok <img alt="" src="/static/icons/icons8-tiktok-96.png" style="width: 32px !important; height: 32px !important;"></a></li>
+        </ul>
+    </div>
+    <div style="width: 25%;" class="w-sm-100 d-flex align-items-end text-end flex-column fir">
+        <h5>Contact</h5>
+        <ul class="nav flex-column">
+            <li class="footer-item mb-2"><a target="_blank" href="https://www.instagram.com/tea_borgs" class="nav-link p-0 text-body-secondary disabled invisible">.</a></li>
+            <li class="footer-item mb-2"><a target="_blank" href="mailto:teaborgs26@gmail.com" class="nav-link p-0 text-body-secondary">E-mail: teaborgs26@gmail.com <img alt="" src="/static/icons/icons8-email-96.png" style="width: 32px !important; height: 32px !important;"></a></li>
+            <li class="footer-item mb-2"><a target="_blank" href="https://goo.gl/maps/t5atCRuUXgEtuBpX9" class="nav-link p-0 text-body-secondary"><?php echo ($l == 1) ? "Strada Unirii" : "Union Street"; ?>, Târgu Jiu 210007 <img alt="" src="/static/icons/icons8-address-96.png" style="width: 32px !important; height: 32px !important;"></a></li>
+        </ul>
+    </div>
+    <div style="width: 25%;" class="w-sm-100 d-flex align-items-end text-end flex-column fir">
+        <h5><?php echo ($l == 1) ? "Dezvoltat de" : "Developed by"; ?></h5>
+        <ul class="nav flex-column">
+            <li class="footer-item mb-2"><a target="_blank" href="" class="nav-link p-0 text-body-secondary disabled invisible">.</a></li>
+            <li class="footer-item mb-2"><a target="_blank" href="https://codulluiandrei.ro/" class="nav-link p-0 text-body-secondary">@andreifrintu <img alt="" src="/static/icons/icons8-code-96.png" style="width: 32px !important; height: 32px !important;"></a></li>
+            <li class="footer-item mb-2"><a target="_blank" href="/team/2024#Andrei Frîntu" class="nav-link p-0 text-body-secondary"><?php echo ($l == 1) ? "din" : "from"; ?> Tea Borgs'24 <img alt="" src="/static/icons/icons8-team-96.png" style="width: 32px !important; height: 32px !important;"></a></li>
+        </ul>
+    </div>
+</div>
+<div style="overflow:hidden;resize:none;max-width:100%;width:75%;height:300px;margin-bottom:2rem;margin-left:auto;margin-right:auto;"><div id="embed-map-display" style="height:100%; width:100%;max-width:100%;"><iframe style="height:100%;width:100%;border:0;" frameborder="0" src="https://www.google.com/maps/embed/v1/place?q=Colegiul+Național+Tudor+Vladimirescu,+Strada+Unirii,+Târgu+Jiu,+România&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"></iframe></div><a class="google-maps-html" rel="nofollow" href="https://www.bootstrapskins.com/themes" id="grab-maps-authorization">premium bootstrap themes</a><style>#embed-map-display img.text-marker{max-width:none!important;background:none!important;}img{max-width:none}</style></div>
---
+++ static/pages/navbar.php
@@ -0,0 +1,38 @@
+<?php
+    /*
+    
+        Website developed by Andrei Frîntu
+        Critical maintenance @ codulluiandrei.ro
+
+        Version:    3.7@main:tb
+        Stage:      published@teaborgs.ro
+        Contact:    admin@codulluiandrei.ro
+
+    */
+?>
+<nav class="navbar navbar-expand-lg navbar-light" style="background: #fcfaf9ff !important; z-index: 999;">
+    <div class="container">
+        <div class="d-flex align-items-center fit">
+            <img alt="" src="/static/logo/logo-2024.png" width="80px">
+        </div>
+        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvas_navbar" aria-controls="offcanvas_navbar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="offcanvas offcanvas-top h-100 bg-transparent" style="width: 100vw;" tabindex="-1" id="offcanvas_navbar">
+            <div class="offcanvas-body d-flex justify-cotent-center align-items-center bg-dark-transparent">
+                <ul class="navbar-nav justify-content-end text-center w-100 flex-grow-1 fs-5">
+                    <li class="nav-item text-center d-md-none"><button type="button" class="btn-close btn-close-white text-center" data-bs-dismiss="offcanvas"></button></li>
+                    <li class="nav-item rounded fit"><a class="nav-link text-md-white text-md-lg" href="/"><?php echo ($l == 1) ? "Acasă" : "Home"; ?></a></li>
+                    <li class="nav-item rounded fit"><a class="nav-link text-md-white text-md-lg" href="/about/"><?php echo ($l == 1) ? "Despre" : "About"; ?></a></li>
+                    <li class="nav-item rounded fit"><a class="nav-link text-md-white text-md-lg" href="/awards/2023"><?php echo ($l == 1) ? "Premii" : "Awards"; ?></a></li>
+                    <li class="nav-item rounded fit"><a class="nav-link text-md-white text-md-lg" href="/team/2024"><?php echo ($l == 1) ? "Echipe" : "Teams"; ?></a></li>
+                    <li class="nav-item rounded fit"><a class="nav-link text-md-white text-md-lg" href="/gallery/"><?php echo ($l == 1) ? "Galerie" : "Gallery"; ?></a></li>
+                    <li class="nav-item rounded fit"><a class="nav-link text-md-white text-md-lg" href="/sponsors/2023"><?php echo ($l == 1) ? "Sponsori" : "Sponsors"; ?></a></li>
+                    <li class="nav-item rounded fit"><a class="nav-link text-md-white text-md-lg" href="/map/"><?php echo ($l == 1) ? "Hartă FTC" : "FTC Map"; ?></a></li>
+                    <li class="nav-item rounded fit"><a class="nav-link text-md-white text-md-lg" href="/news/"><?php echo ($l == 1) ? "Știri & Evenimente" : "News & Events"; ?></a></li>
+                    <li class="rounded fit"><img src="<?php echo ($l == 1) ? "/static/icons/icons8-great-britain.png" : "/static/icons/icons8-romania.png"; ?>" id="swapLanguage" onclick="swapLanguage()" style="width: 50px; height: 50px; cursor: pointer;" class="rounded"></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</nav>
